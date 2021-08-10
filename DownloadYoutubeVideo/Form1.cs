8d91dc91fe43617c673aaf78f1f01659f4207c65
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-08-10 16:47:50 -0600

    Adding material design configurations.

--- DownloadYoutubeVideo/Form1.cs
+++ DownloadYoutubeVideo/Form1.cs
@@ -7,14 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin.Controls;
 
 namespace DownloadYoutubeVideo
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
+        private readonly MaterialSkin.MaterialSkinManager _materialSkinManager;
         public Form1()
         {
             InitializeComponent();
+            _materialSkinManager = MaterialSkin.MaterialSkinManager.Instance;
+            _materialSkinManager.EnforceBackcolorOnAllComponents = true;
+            _materialSkinManager.AddFormToManage(this);
+            _materialSkinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
+            _materialSkinManager.ColorScheme = new MaterialSkin.ColorScheme(MaterialSkin.Primary.Indigo500, MaterialSkin.Primary.Indigo700, MaterialSkin.Primary.Indigo100,MaterialSkin.Accent.Pink200, MaterialSkin.TextShade.WHITE);
         }
 
     }
