2628ed89a543e2a0c989bd9a888cc504669872e5
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-08-10 17:14:46 -0600

    Adding icons in the tab page.

--- DownloadYoutubeVideo/Form1.Designer.cs
+++ DownloadYoutubeVideo/Form1.Designer.cs
@@ -29,20 +29,108 @@ namespace DownloadYoutubeVideo
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
+            this.tabHome = new System.Windows.Forms.TabPage();
+            this.tabUser = new System.Windows.Forms.TabPage();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.tabOnlineSupport = new System.Windows.Forms.TabPage();
+            this.tabSettings = new System.Windows.Forms.TabPage();
+            this.materialTabControl1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // materialTabControl1
+            // 
+            this.materialTabControl1.Controls.Add(this.tabHome);
+            this.materialTabControl1.Controls.Add(this.tabUser);
+            this.materialTabControl1.Controls.Add(this.tabOnlineSupport);
+            this.materialTabControl1.Controls.Add(this.tabSettings);
+            this.materialTabControl1.Depth = 0;
+            this.materialTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.materialTabControl1.ImageList = this.imageList1;
+            this.materialTabControl1.Location = new System.Drawing.Point(3, 64);
+            this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabControl1.Multiline = true;
+            this.materialTabControl1.Name = "materialTabControl1";
+            this.materialTabControl1.SelectedIndex = 0;
+            this.materialTabControl1.Size = new System.Drawing.Size(704, 407);
+            this.materialTabControl1.TabIndex = 0;
+            // 
+            // tabHome
+            // 
+            this.tabHome.BackColor = System.Drawing.Color.White;
+            this.tabHome.ImageKey = "home_32px.png";
+            this.tabHome.Location = new System.Drawing.Point(4, 39);
+            this.tabHome.Name = "tabHome";
+            this.tabHome.Padding = new System.Windows.Forms.Padding(3);
+            this.tabHome.Size = new System.Drawing.Size(696, 364);
+            this.tabHome.TabIndex = 0;
+            this.tabHome.Text = "Home";
+            // 
+            // tabUser
+            // 
+            this.tabUser.BackColor = System.Drawing.Color.White;
+            this.tabUser.ImageKey = "add_user_male_32px.png";
+            this.tabUser.Location = new System.Drawing.Point(4, 39);
+            this.tabUser.Name = "tabUser";
+            this.tabUser.Padding = new System.Windows.Forms.Padding(3);
+            this.tabUser.Size = new System.Drawing.Size(696, 364);
+            this.tabUser.TabIndex = 1;
+            this.tabUser.Text = "User";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "add_user_male_32px.png");
+            this.imageList1.Images.SetKeyName(1, "home_32px.png");
+            this.imageList1.Images.SetKeyName(2, "online_support_32px.png");
+            this.imageList1.Images.SetKeyName(3, "settings_32px.png");
+            // 
+            // tabOnlineSupport
+            // 
+            this.tabOnlineSupport.BackColor = System.Drawing.Color.White;
+            this.tabOnlineSupport.ImageKey = "online_support_32px.png";
+            this.tabOnlineSupport.Location = new System.Drawing.Point(4, 39);
+            this.tabOnlineSupport.Name = "tabOnlineSupport";
+            this.tabOnlineSupport.Size = new System.Drawing.Size(696, 364);
+            this.tabOnlineSupport.TabIndex = 2;
+            this.tabOnlineSupport.Text = "Online Support";
+            // 
+            // tabSettings
+            // 
+            this.tabSettings.BackColor = System.Drawing.Color.White;
+            this.tabSettings.ImageKey = "settings_32px.png";
+            this.tabSettings.Location = new System.Drawing.Point(4, 39);
+            this.tabSettings.Name = "tabSettings";
+            this.tabSettings.Size = new System.Drawing.Size(696, 364);
+            this.tabSettings.TabIndex = 3;
+            this.tabSettings.Text = "Settings";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(696, 306);
+            this.ClientSize = new System.Drawing.Size(710, 474);
+            this.Controls.Add(this.materialTabControl1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Download Youtube Video";
+            this.materialTabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
+        private System.Windows.Forms.TabPage tabHome;
+        private System.Windows.Forms.TabPage tabUser;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TabPage tabOnlineSupport;
+        private System.Windows.Forms.TabPage tabSettings;
     }
 }
 
