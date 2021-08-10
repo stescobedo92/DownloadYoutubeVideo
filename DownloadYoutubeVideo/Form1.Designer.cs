938c8adcb4c721004f4fdb3e354e0a74f7637902
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-08-10 17:45:03 -0600

    Resize

--- DownloadYoutubeVideo/Form1.Designer.cs
+++ DownloadYoutubeVideo/Form1.Designer.cs
@@ -33,6 +33,16 @@ namespace DownloadYoutubeVideo
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
             this.tabHome = new System.Windows.Forms.TabPage();
+            this.materialLblSpeed = new MaterialSkin.Controls.MaterialLabel();
+            this.lblSpeed = new MaterialSkin.Controls.MaterialLabel();
+            this.lblDownloaded = new MaterialSkin.Controls.MaterialLabel();
+            this.labelDownload = new MaterialSkin.Controls.MaterialLabel();
+            this.btnResume = new MaterialSkin.Controls.MaterialButton();
+            this.btnPause = new MaterialSkin.Controls.MaterialButton();
+            this.percentLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.progressBar = new MaterialSkin.Controls.MaterialProgressBar();
+            this.statusLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
             this.txtUrl = new MaterialSkin.Controls.MaterialTextBox();
             this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
             this.btnStart = new MaterialSkin.Controls.MaterialButton();
@@ -40,16 +50,6 @@ namespace DownloadYoutubeVideo
             this.tabOnlineSupport = new System.Windows.Forms.TabPage();
             this.tabSettings = new System.Windows.Forms.TabPage();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
-            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
-            this.statusLabel = new MaterialSkin.Controls.MaterialLabel();
-            this.progressBar = new MaterialSkin.Controls.MaterialProgressBar();
-            this.percentLabel = new MaterialSkin.Controls.MaterialLabel();
-            this.btnPause = new MaterialSkin.Controls.MaterialButton();
-            this.btnResume = new MaterialSkin.Controls.MaterialButton();
-            this.lblDownloaded = new MaterialSkin.Controls.MaterialLabel();
-            this.labelDownload = new MaterialSkin.Controls.MaterialLabel();
-            this.lblSpeed = new MaterialSkin.Controls.MaterialLabel();
-            this.materialLblSpeed = new MaterialSkin.Controls.MaterialLabel();
             this.materialTabControl1.SuspendLayout();
             this.tabHome.SuspendLayout();
             this.SuspendLayout();
@@ -68,7 +68,7 @@ namespace DownloadYoutubeVideo
             this.materialTabControl1.Multiline = true;
             this.materialTabControl1.Name = "materialTabControl1";
             this.materialTabControl1.SelectedIndex = 0;
-            this.materialTabControl1.Size = new System.Drawing.Size(704, 407);
+            this.materialTabControl1.Size = new System.Drawing.Size(715, 407);
             this.materialTabControl1.TabIndex = 0;
             // 
             // tabHome
@@ -91,10 +91,140 @@ namespace DownloadYoutubeVideo
             this.tabHome.Location = new System.Drawing.Point(4, 39);
             this.tabHome.Name = "tabHome";
             this.tabHome.Padding = new System.Windows.Forms.Padding(3);
-            this.tabHome.Size = new System.Drawing.Size(696, 364);
+            this.tabHome.Size = new System.Drawing.Size(707, 364);
             this.tabHome.TabIndex = 0;
             this.tabHome.Text = "Home";
             this.tabHome.Click += new System.EventHandler(this.tabHome_Click);
+            // 
+            // materialLblSpeed
+            // 
+            this.materialLblSpeed.AutoSize = true;
+            this.materialLblSpeed.Depth = 0;
+            this.materialLblSpeed.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLblSpeed.Location = new System.Drawing.Point(120, 184);
+            this.materialLblSpeed.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLblSpeed.Name = "materialLblSpeed";
+            this.materialLblSpeed.Size = new System.Drawing.Size(53, 19);
+            this.materialLblSpeed.TabIndex = 12;
+            this.materialLblSpeed.Text = "0 MB/s";
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.AutoSize = true;
+            this.lblSpeed.Depth = 0;
+            this.lblSpeed.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.lblSpeed.Location = new System.Drawing.Point(64, 183);
+            this.lblSpeed.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(49, 19);
+            this.lblSpeed.TabIndex = 11;
+            this.lblSpeed.Text = "Speed:";
+            // 
+            // lblDownloaded
+            // 
+            this.lblDownloaded.AutoSize = true;
+            this.lblDownloaded.Depth = 0;
+            this.lblDownloaded.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.lblDownloaded.Location = new System.Drawing.Point(119, 157);
+            this.lblDownloaded.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lblDownloaded.Name = "lblDownloaded";
+            this.lblDownloaded.Size = new System.Drawing.Size(38, 19);
+            this.lblDownloaded.TabIndex = 10;
+            this.lblDownloaded.Text = "0 MB";
+            // 
+            // labelDownload
+            // 
+            this.labelDownload.AutoSize = true;
+            this.labelDownload.Depth = 0;
+            this.labelDownload.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.labelDownload.Location = new System.Drawing.Point(19, 157);
+            this.labelDownload.MouseState = MaterialSkin.MouseState.HOVER;
+            this.labelDownload.Name = "labelDownload";
+            this.labelDownload.Size = new System.Drawing.Size(94, 19);
+            this.labelDownload.TabIndex = 9;
+            this.labelDownload.Text = "Downloaded:";
+            // 
+            // btnResume
+            // 
+            this.btnResume.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnResume.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnResume.Depth = 0;
+            this.btnResume.HighEmphasis = true;
+            this.btnResume.Icon = null;
+            this.btnResume.Location = new System.Drawing.Point(600, 147);
+            this.btnResume.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnResume.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(88, 36);
+            this.btnResume.TabIndex = 8;
+            this.btnResume.Text = "&Resume";
+            this.btnResume.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnResume.UseAccentColor = false;
+            this.btnResume.UseVisualStyleBackColor = true;
+            // 
+            // btnPause
+            // 
+            this.btnPause.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnPause.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnPause.Depth = 0;
+            this.btnPause.HighEmphasis = true;
+            this.btnPause.Icon = null;
+            this.btnPause.Location = new System.Drawing.Point(516, 147);
+            this.btnPause.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnPause.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(76, 36);
+            this.btnPause.TabIndex = 7;
+            this.btnPause.Text = "&Pause";
+            this.btnPause.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnPause.UseAccentColor = false;
+            this.btnPause.UseVisualStyleBackColor = true;
+            // 
+            // percentLabel
+            // 
+            this.percentLabel.AutoSize = true;
+            this.percentLabel.Depth = 0;
+            this.percentLabel.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.percentLabel.Location = new System.Drawing.Point(654, 101);
+            this.percentLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.percentLabel.Name = "percentLabel";
+            this.percentLabel.Size = new System.Drawing.Size(22, 19);
+            this.percentLabel.TabIndex = 6;
+            this.percentLabel.Text = "0%";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Depth = 0;
+            this.progressBar.Location = new System.Drawing.Point(79, 123);
+            this.progressBar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(600, 5);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 5;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Depth = 0;
+            this.statusLabel.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.statusLabel.Location = new System.Drawing.Point(81, 87);
+            this.statusLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(25, 19);
+            this.statusLabel.TabIndex = 4;
+            this.statusLabel.Text = "???";
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel2.Location = new System.Drawing.Point(16, 87);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(51, 19);
+            this.materialLabel2.TabIndex = 3;
+            this.materialLabel2.Text = "Status:";
             // 
             // txtUrl
             // 
@@ -102,7 +232,7 @@ namespace DownloadYoutubeVideo
             this.txtUrl.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtUrl.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.txtUrl.Depth = 0;
-            this.txtUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.txtUrl.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
             this.txtUrl.LeadingIcon = null;
             this.txtUrl.Location = new System.Drawing.Point(79, 9);
             this.txtUrl.MaxLength = 50;
@@ -185,141 +315,11 @@ namespace DownloadYoutubeVideo
             this.imageList1.Images.SetKeyName(2, "online_support_32px.png");
             this.imageList1.Images.SetKeyName(3, "settings_32px.png");
             // 
-            // materialLabel2
-            // 
-            this.materialLabel2.AutoSize = true;
-            this.materialLabel2.Depth = 0;
-            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.materialLabel2.Location = new System.Drawing.Point(16, 87);
-            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialLabel2.Name = "materialLabel2";
-            this.materialLabel2.Size = new System.Drawing.Size(51, 19);
-            this.materialLabel2.TabIndex = 3;
-            this.materialLabel2.Text = "Status:";
-            // 
-            // statusLabel
-            // 
-            this.statusLabel.AutoSize = true;
-            this.statusLabel.Depth = 0;
-            this.statusLabel.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.statusLabel.Location = new System.Drawing.Point(81, 87);
-            this.statusLabel.MouseState = MaterialSkin.MouseState.HOVER;
-            this.statusLabel.Name = "statusLabel";
-            this.statusLabel.Size = new System.Drawing.Size(25, 19);
-            this.statusLabel.TabIndex = 4;
-            this.statusLabel.Text = "???";
-            // 
-            // progressBar
-            // 
-            this.progressBar.Depth = 0;
-            this.progressBar.Location = new System.Drawing.Point(79, 123);
-            this.progressBar.MouseState = MaterialSkin.MouseState.HOVER;
-            this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(600, 5);
-            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
-            this.progressBar.TabIndex = 5;
-            // 
-            // percentLabel
-            // 
-            this.percentLabel.AutoSize = true;
-            this.percentLabel.Depth = 0;
-            this.percentLabel.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.percentLabel.Location = new System.Drawing.Point(654, 101);
-            this.percentLabel.MouseState = MaterialSkin.MouseState.HOVER;
-            this.percentLabel.Name = "percentLabel";
-            this.percentLabel.Size = new System.Drawing.Size(22, 19);
-            this.percentLabel.TabIndex = 6;
-            this.percentLabel.Text = "0%";
-            // 
-            // btnPause
-            // 
-            this.btnPause.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnPause.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
-            this.btnPause.Depth = 0;
-            this.btnPause.HighEmphasis = true;
-            this.btnPause.Icon = null;
-            this.btnPause.Location = new System.Drawing.Point(516, 147);
-            this.btnPause.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
-            this.btnPause.MouseState = MaterialSkin.MouseState.HOVER;
-            this.btnPause.Name = "btnPause";
-            this.btnPause.Size = new System.Drawing.Size(76, 36);
-            this.btnPause.TabIndex = 7;
-            this.btnPause.Text = "&Pause";
-            this.btnPause.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
-            this.btnPause.UseAccentColor = false;
-            this.btnPause.UseVisualStyleBackColor = true;
-            // 
-            // btnResume
-            // 
-            this.btnResume.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnResume.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
-            this.btnResume.Depth = 0;
-            this.btnResume.HighEmphasis = true;
-            this.btnResume.Icon = null;
-            this.btnResume.Location = new System.Drawing.Point(600, 147);
-            this.btnResume.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
-            this.btnResume.MouseState = MaterialSkin.MouseState.HOVER;
-            this.btnResume.Name = "btnResume";
-            this.btnResume.Size = new System.Drawing.Size(88, 36);
-            this.btnResume.TabIndex = 8;
-            this.btnResume.Text = "&Resume";
-            this.btnResume.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
-            this.btnResume.UseAccentColor = false;
-            this.btnResume.UseVisualStyleBackColor = true;
-            // 
-            // lblDownloaded
-            // 
-            this.lblDownloaded.AutoSize = true;
-            this.lblDownloaded.Depth = 0;
-            this.lblDownloaded.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.lblDownloaded.Location = new System.Drawing.Point(119, 157);
-            this.lblDownloaded.MouseState = MaterialSkin.MouseState.HOVER;
-            this.lblDownloaded.Name = "lblDownloaded";
-            this.lblDownloaded.Size = new System.Drawing.Size(38, 19);
-            this.lblDownloaded.TabIndex = 10;
-            this.lblDownloaded.Text = "0 MB";
-            // 
-            // labelDownload
-            // 
-            this.labelDownload.AutoSize = true;
-            this.labelDownload.Depth = 0;
-            this.labelDownload.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.labelDownload.Location = new System.Drawing.Point(19, 157);
-            this.labelDownload.MouseState = MaterialSkin.MouseState.HOVER;
-            this.labelDownload.Name = "labelDownload";
-            this.labelDownload.Size = new System.Drawing.Size(94, 19);
-            this.labelDownload.TabIndex = 9;
-            this.labelDownload.Text = "Downloaded:";
-            // 
-            // lblSpeed
-            // 
-            this.lblSpeed.AutoSize = true;
-            this.lblSpeed.Depth = 0;
-            this.lblSpeed.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.lblSpeed.Location = new System.Drawing.Point(64, 183);
-            this.lblSpeed.MouseState = MaterialSkin.MouseState.HOVER;
-            this.lblSpeed.Name = "lblSpeed";
-            this.lblSpeed.Size = new System.Drawing.Size(49, 19);
-            this.lblSpeed.TabIndex = 11;
-            this.lblSpeed.Text = "Speed:";
-            // 
-            // materialLblSpeed
-            // 
-            this.materialLblSpeed.AutoSize = true;
-            this.materialLblSpeed.Depth = 0;
-            this.materialLblSpeed.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.materialLblSpeed.Location = new System.Drawing.Point(120, 184);
-            this.materialLblSpeed.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialLblSpeed.Name = "materialLblSpeed";
-            this.materialLblSpeed.Size = new System.Drawing.Size(53, 19);
-            this.materialLblSpeed.TabIndex = 12;
-            this.materialLblSpeed.Text = "0 MB/s";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(710, 474);
+            this.ClientSize = new System.Drawing.Size(721, 474);
             this.Controls.Add(this.materialTabControl1);
             this.DrawerShowIconsWhenHidden = true;
             this.DrawerTabControl = this.materialTabControl1;
